bf85db85d19a0fc7e09cb19a9253758208fc01ca
vithanidarpan <darpan@canopas.com>
2020-11-24 18:15:01 +0530

    Update docker-compose.yaml

--- db.go
+++ db.go
@@ -10,19 +10,17 @@ import (
 var AppDb *gorm.DB = nil
 
 func InitDb() (*gorm.DB, bool) {
-	dbUsername := os.Getenv("MYSQL_ROOT_USER")
-	dbPassword := os.Getenv("MYSQL_ROOT_PASSWORD")
+	dbUsername := os.Getenv("MYSQL_USER")
+	dbPassword := os.Getenv("MYSQL_PASSWORD")
 	dbHost := os.Getenv("MYSQL_HOST")
 	dbPort := os.Getenv("MYSQL_PORT")
-	dbName := os.Getenv("MYSQL_DATABASE_NAME")
+	dbName := os.Getenv("MYSQL_DATABASE")
 
 	var err error
 
 	dsn := dbUsername + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?parseTime=true&&loc=Local"
 	AppDb, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	//dsn := "user=isystech password=itsmeilhem dbname=gifts_winner port=5432 sslmode=disable TimeZone=Asia/Jerusalem"
-	//AppDb, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	AppDb.LogMode(true)
+
 	if err != nil {
 		fmt.Println("InitDb(): Unable to Connect to Database: error=%v", err)
 		return nil, false
